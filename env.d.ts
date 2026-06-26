c5615da770800e167186d0c6238571943999a8a6
Nelson Flores <nelson.flores@live.com>
2026-06-26 06:51:46 +0200

    updates

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+declare module '*.js' {
+  const value: any
+  export default value
+}
